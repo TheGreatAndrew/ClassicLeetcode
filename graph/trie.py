18a340185076f856f0c045b4de4b4275acabd746
andrew <andrewvu0203@gmail.com>
2022-06-19 07:56:12 -0500

    june 18

--- graph/trie.py
+++
@@ -1,4 +0,0 @@
-# trie (prefix tree) is type of tree, often for characters
-
-# https://leetcode.com/problems/implement-trie-prefix-tree/ solve half of it myself
-# insert trie : get node/or create node, replace it, and move on
