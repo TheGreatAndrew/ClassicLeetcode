0187ee112ae0524f14e86ab115b5369dd9703de9
andrew <andrewvu0203@gmail.com>
2022-07-01 22:31:23 -0500

    1 hard problem + 2 guide

--- heap/cheatsheet.py
+++
@@ -1,5 +0,0 @@
-# heap
-# insert -> insert next element top-bottom, left-right. then heapify up
-# pop -> pop min root, swapped with last added element. then heapify down
-# heapify -> bubble up until parent is no longer smaller. or bubble down the element 
-# can be array  
--- linkedlist/cheatsheet.py
+++
@@ -1,7 +0,0 @@
-# 2 pointers
-def merge() :
-    dummy = cur = ListNode(0)
-    return dummy.next
-
-
-# LRU cache
